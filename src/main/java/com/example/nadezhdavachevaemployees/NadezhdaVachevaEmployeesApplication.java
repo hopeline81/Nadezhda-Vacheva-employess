9cfc367ab37dd8502cf14c132041b467524e04e5
Nadezhda <nadezhda.vacheva@volasoftware.com>
2024-07-10 11:01:42 +0300

    add main folders

---
+++ src/main/java/com/example/nadezhdavachevaemployees/NadezhdaVachevaEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.example.nadezhdavachevaemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadezhdaVachevaEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadezhdaVachevaEmployeesApplication.class, args);
+	}
+
+}
